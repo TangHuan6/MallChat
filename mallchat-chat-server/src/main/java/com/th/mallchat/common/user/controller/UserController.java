f48d56fe2fcb216d2a9a056d2ca5ea63f9ecf415
th <2938594359@qq.com>
2025-05-19 16:41:00 +0800

    实现握手认证

--- mallchat-chat-server/src/main/java/com/th/mallchat/common/user/controller/UserController.java
+++ mallchat-chat-server/src/main/java/com/th/mallchat/common/user/controller/UserController.java
@@ -1,17 +1,24 @@
 package com.th.mallchat.common.user.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/user")
-@ResponseBody
+@Slf4j
+@RequestMapping("/capi/user")
+@RestController
 public class UserController {
 
     @GetMapping("/test")
     public String index() {
         return "hello";
+    }
+
+    @GetMapping("/userInfo")
+    public void getUserInfo(){
+
     }
 }
